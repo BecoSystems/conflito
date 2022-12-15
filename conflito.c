e8f7a11dd5ea3a4d2beee7e6a9afd672886dc7ec
Leonardo Correia Santos Galvao <lcsg@poli.br>
2022-12-15 15:03:10 -0300

    conflito:Leonardo Correia Santos Galvao

--- conflito.c
+++ conflito.c
@@ -77,7 +77,7 @@ int f0(int x)
 }
 
 /* ---------------------------------------------------------------------- */
-/* f10 Nome completo, criar corpo */
+/* f10 Leonardo Correia Santos Galvao */
 
 /* ---------------------------------------------------------------------- */
 /* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
