dae0dc9e4ef6eef8015804898d7fc7b2af269b9e
Leonardo Correia Santos Galvao <lcsg@poli.br>
2023-06-29 15:02:46 -0300

    conflito: Leonardo Correia Santos Galvao adicionado a f14

--- conflito.c
+++ conflito.c
@@ -130,7 +130,22 @@ int f13 (int x)
 
    return y;
 }
+/*----------------------------------------------------------------------- */
+/* f14 Leonardo */
+int f14 (void){
 
+    int numeros;
+
+
+    printf("Digite qualquer numero da sua escolha:\n ");
+    scanf("%d", &numeros);
+    printf(" Onumero escolhido foi:\n " "%d", numeros);
+
+    return 0;
+
+
+}
+/*----------------------------------------------------------------------- */
 /* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
 /* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
 
